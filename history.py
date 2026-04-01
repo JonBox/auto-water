1ae8de5154dbfb181910d830c5157d51ac3f9081
JonBox <jonathan.r.blanton@gmail.com>
2026-04-01 07:56:40 -0400

    Excluding charting for now

--- history.py
+++ history.py
@@ -1,9 +1,9 @@
 import csv
 from datetime import datetime
-import matplotlib.pyplot as plt
-import io
-import matplotlib.dates as mdates
-from PIL import Image
+# import matplotlib.pyplot as plt
+# import io
+# import matplotlib.dates as mdates
+# from PIL import Image
 
 class history:
     def __init__(self):
@@ -30,54 +30,54 @@ class history:
         status_file = open(self.filename, "r")
         return status_file.read()
 
-    def fig2img(self, fig):
-        buf = io.BytesIO()
-        fig.savefig(buf)
-        buf.seek(0)
-        img = Image.open(buf)
-        return img
-
-    def chart_data(self):
-        dates = []
-        is_on = []
-        values = []
-        voltages = []
-        status_file = open(self.filename, "r")
-        reader = csv.reader(status_file, delimiter=' ', quotechar='|')
-
-        rowcount = 0
-        for row in reader:
-            rowcount += 1
-            # skip header row
-            if rowcount == 1:
-                continue
-            # assemble data
-            dates.append(datetime.fromisoformat(row[0]))
-            is_on.append(1 if row[1] == 'True' else 0)
-            values.append(float(row[2]))
-            voltages.append(float(row[3]))
-        # Plotting Line Graph
-        plt.figure()
-        plt.title("Watering and Soil Moisture")
-        plt.xlabel('Date')
-        plt.plot(dates, is_on, label='Water On', scaley=True)
-        #plt.plot(dates, values, '-.', label='Moisture Value', scaley=True)
-        plt.plot(dates, voltages, '-.', label='Sensor Voltage', scaley=True)
-        plt.legend()
-
-        ax = plt.gca()
-        locator = mdates.AutoDateLocator()
-        formatter = mdates.ConciseDateFormatter(locator)
-        ax.xaxis.set_major_locator(locator)
-        ax.xaxis.set_major_formatter(formatter)
-        plt.gcf().autofmt_xdate()
-
-        # Getting the current figure and save it in the variable.
-        fig = plt.gcf()
-
-        # Create a Function for Converting a figure to a PIL Image.
-        img = self.fig2img(fig)
-        plt.close(fig)
-        image_filename = f'{datetime.now()}.png'
-        img.save(image_filename)
-        return image_filename
+    # def fig2img(self, fig):
+    #     buf = io.BytesIO()
+    #     fig.savefig(buf)
+    #     buf.seek(0)
+    #     img = Image.open(buf)
+    #     return img
+    #
+    # def chart_data(self):
+    #     dates = []
+    #     is_on = []
+    #     values = []
+    #     voltages = []
+    #     status_file = open(self.filename, "r")
+    #     reader = csv.reader(status_file, delimiter=' ', quotechar='|')
+    #
+    #     rowcount = 0
+    #     for row in reader:
+    #         rowcount += 1
+    #         # skip header row
+    #         if rowcount == 1:
+    #             continue
+    #         # assemble data
+    #         dates.append(datetime.fromisoformat(row[0]))
+    #         is_on.append(1 if row[1] == 'True' else 0)
+    #         values.append(float(row[2]))
+    #         voltages.append(float(row[3]))
+    #     # Plotting Line Graph
+    #     plt.figure()
+    #     plt.title("Watering and Soil Moisture")
+    #     plt.xlabel('Date')
+    #     plt.plot(dates, is_on, label='Water On', scaley=True)
+    #     #plt.plot(dates, values, '-.', label='Moisture Value', scaley=True)
+    #     plt.plot(dates, voltages, '-.', label='Sensor Voltage', scaley=True)
+    #     plt.legend()
+    #
+    #     ax = plt.gca()
+    #     locator = mdates.AutoDateLocator()
+    #     formatter = mdates.ConciseDateFormatter(locator)
+    #     ax.xaxis.set_major_locator(locator)
+    #     ax.xaxis.set_major_formatter(formatter)
+    #     plt.gcf().autofmt_xdate()
+    #
+    #     # Getting the current figure and save it in the variable.
+    #     fig = plt.gcf()
+    #
+    #     # Create a Function for Converting a figure to a PIL Image.
+    #     img = self.fig2img(fig)
+    #     plt.close(fig)
+    #     image_filename = f'{datetime.now()}.png'
+    #     img.save(image_filename)
+    #     return image_filename
