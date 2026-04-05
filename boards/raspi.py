65440a339febe26a74a5fe1030201c3b885b062d
JonBox <jonathan.r.blanton@gmail.com>
2026-04-05 10:54:06 -0400

    Adding i2c for raspi

--- boards/raspi.py
+++ boards/raspi.py
@@ -1,12 +1,19 @@
 from boards.hardware import hardware
 from gpiozero import OutputDevice
+from adafruit_ads1x15 import AnalogIn, ads1115
+import busio
+import adafruit_ads1x15.ads1x15 as ADS
+import board
+
 
 class raspi(hardware):
     def __init__(self):
-        self.output = OutputDevice(5, active_high=False)
-        pass
+        self.i2c = busio.I2C(board.SCL, board.SDA)  # uses board.SCL and board.SDA
+        self.ads = ads1115.ADS1115(self.i2c)
+        self.chan = AnalogIn(self.ads, ADS.Pin.A0)
+        self.output = OutputDevice(5, active_high=True)
     def get_analog_input(self):
-        return None
+        return self.chan.value, self.chan.voltage
     def turn_digital_output_on(self):
         self.output.on()
     def turn_digital_output_off(self):
