15f45c8f4d086a1d421fea3d498f1903dcb50394
JonBox <jonathan.r.blanton@gmail.com>
2026-05-18 18:30:02 -0400

    Fixing water time bug

--- outputs/water_servo.py
+++ outputs/water_servo.py
@@ -6,19 +6,19 @@ class water_servo:
         self.board = board
         self.watering_time = 60*15 # 15min
         self.watering_started_at = datetime.now()
+        self.watering_ends_at = None
 
-    def turn_on(self):
+    def turn_on(self, watering_time = None):
         self.board.turn_digital_output_on()
         self.watering_started_at = datetime.now()
-
+        self.watering_ends_at = self.watering_started_at + timedelta(seconds=watering_time)
     def turn_off(self):
         self.board.turn_digital_output_off()
 
     def is_on(self):
         return self.board.digital_output_is_on()
 
-    def auto_off(self, watering_time = None):
-        watering_time = watering_time or self.watering_time
-        if self.board.digital_output_is_on() and self.watering_started_at < (datetime.now() - timedelta(seconds=watering_time)):
+    def auto_off(self):
+        self.watering_started_at = datetime.now()
+        if self.board.digital_output_is_on() and self.watering_ends_at < datetime.now():
             self.board.turn_digital_output_off()
-
