4dcb8348cc1cbdad50e35f2e77c8fd61948ff1f1
JonBox <jonathan.r.blanton@gmail.com>
2026-06-21 17:57:45 -0400

    Logging

--- outputs/water_servo.py
+++ outputs/water_servo.py
@@ -9,10 +9,13 @@ class water_servo:
         self.watering_ends_at = None
 
     def turn_on(self, watering_time = None):
+        print(f"Watering for {watering_time} seconds")
         self.board.turn_digital_output_on()
         self.watering_started_at = datetime.now()
         self.watering_ends_at = self.watering_started_at + timedelta(seconds=watering_time)
+
     def turn_off(self):
+        print(f"Watering stopped")
         self.board.turn_digital_output_off()
 
     def is_on(self):
@@ -21,4 +24,5 @@ class water_servo:
     def auto_off(self):
         self.watering_started_at = datetime.now()
         if self.board.digital_output_is_on() and self.watering_ends_at < datetime.now():
+            print(f"Auto-off triggered")
             self.board.turn_digital_output_off()
